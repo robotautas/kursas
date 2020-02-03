514bff90b7ce0127dd4ceeb0ab44516485a1530e
robotautas <50068472+robotautas@users.noreply.github.com>
2020-02-03 16:35:43 +0200

    Update app.py

--- Flask/Flask 2 dalis/MDs/uzduotis/app.py
+++ Flask/Flask 2 dalis/MDs/uzduotis/app.py
@@ -1,24 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from form import ContactForm
-import json
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bet kokia simbolių eilutė'
-
-def append_data(new_data):
-    with open('db.json', 'r') as db:
-        data = json.load(db)
-        data.append(new_data)
-    with open('db.json', 'w') as db:
-        json.dump(data, db)
-
-
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
     form = ContactForm()
     if form.validate_on_submit():
-        append_data(form.data)
         return render_template('success.html', form=form)
     return render_template('form.html', form=form)
 
