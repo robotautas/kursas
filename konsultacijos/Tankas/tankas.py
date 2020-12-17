3425eeaea92f7bd55ff9f449a7715ab4356effcb
robotautas <50068472+robotautas@users.noreply.github.com>
2020-12-17 17:27:07 +0200

    Update tankas.py

--- konsultacijos/Tankas/tankas.py
+++ konsultacijos/Tankas/tankas.py
@@ -1,29 +1,39 @@
+
 from random import randint
+from os import system
+
+_ = system('cls')
 
 class Tankas:
     x = 0
     y = 0
+    taskai = 100
     kryptis = 'n'
+    kryptys = {'n': 'šiaurė', 's': 'pietūs', 'e': 'rytai', 'w': 'vakarai'}
     suviai = {'n': 0, 's': 0, 'e': 0, 'w': 0}
 
     def pirmyn(self):
         self.y += 1
         self.kryptis = 'n'
+        self.taskai -= 10
         print("Pirmyn")
 
     def atgal(self):
         self.y -= 1
         self.kryptis = 's'
+        self.taskai -= 10
         print("Atgal")
 
     def kairėn(self):
         self.x -= 1
         self.kryptis = 'w'
+        self.taskai -= 10
         print("Kairėn")
 
     def dešinėn(self):
         self.x += 1
         self.kryptis = 'e'
+        self.taskai -= 10
         print("Dešinėn")
     
     def nusitaikymas(self, target_x, target_y):
@@ -42,31 +52,28 @@ class Tankas:
 
     def šūvis(self):
         self.suviai[self.kryptis] += 1
-        print("Iššauta")
+        self.taskai -= 10
 
     def info(self):
-        print("Info:")
-        print(f"Kryptis: {self.kryptis}")
-        print(f"Koordinatės: X: {self.x}, Y: {self.y}")
-        print(f"Šūviai: šiaurė: {self.suviai['n']}, pietūs: {self.suviai['s']}, rytai: {self.suviai['e']}, vakarai: {self.suviai['w']}")
-    
+        self.taskai -= 10
+        print("\n")
+        print(f"Kryptis: {self.kryptys[self.kryptis]}")
+        print(f"Tanko koordinatės: {self.x}, {self.y}")
+        print(f"Šūviai: šiaurė: {self.suviai['n']}, pietūs: {self.suviai['s']}, rytai: {self.suviai['e']}, vakarai: {self.suviai['w']}\n")
+
 class Taikinys():
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
-    def get_coords(self):
-        print(f'Taikinys yra {self.x}, {self.y}')
 
 
 tankas = Tankas()
 taikinys = Taikinys(randint(-5, 6), randint(-5, 6))
-taikinys.get_coords()
 
 
 while True:
-    print("Įveskite veiksmą")
-    print("p - pirmyn, a = atgal, k - kairėn, d - dešinėn, suvis, info, end")
+    # print("Įveskite veiksmą")
+    print(f"[{tankas.taskai}][taikinys: {(taikinys.x, taikinys.y)}]\np - pirmyn, a = atgal, k - kairėn, d - dešinėn, suvis, info, end")
     pasirinkimas = input()
     if pasirinkimas == "p":
         tankas.pirmyn()
@@ -80,9 +87,11 @@ while True:
         tankas.šūvis()
         if tankas.nusitaikymas(taikinys.x, taikinys.y):
             print('PATAIKEI!')
+            tankas.taskai += 100
             taikinys = Taikinys(randint(-5, 6), randint(-5, 6))
-            taikinys.get_coords()
+            
         else:
+            tankas.taskai -= 10
             print('NEPATAIKEI!')
     elif pasirinkimas == "info":
         tankas.info()
