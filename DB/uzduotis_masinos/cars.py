04f9521049fb085a2e1f6daa3e2e95b4aa97aa1c
robotautas <50068472+robotautas@users.noreply.github.com>
2021-01-27 16:15:27 +0200

    Uzduotis su masinom DB

---
+++ DB/uzduotis_masinos/cars.py
@@ -0,0 +1,69 @@
+import sqlite3
+from sys import exit
+
+conn = sqlite3.connect('cars.db')
+c = conn.cursor()
+
+insert_query = '''
+INSERT INTO cars VALUES(?,?,?,?,?)
+'''
+
+search_query = '''
+SELECT * FROM cars
+WHERE
+make LIKE ?
+AND
+model LIKE ?
+AND
+color LIKE ?
+AND
+year BETWEEN ? AND ?
+AND
+price BETWEEN ? AND ?
+'''
+
+def insert():
+    make = input('Make: ')
+    model = input('Model: ')
+    color = input('Color: ')
+    year = int(input('Year: '))
+    price = int(input('Price: '))
+    with conn:
+        c.execute(insert_query, (make, model, color, year, price))
+    print(f'{make} {model} ({year}) successfully created!')
+
+def search():
+    make = input('Make: ')
+    model = input('Model: ')
+    color = input('Color: ')
+    year_from = input('Year from: ')
+    year_to = input('Year to: ')
+    price_from = input('Price from: ')
+    price_to = input('Price to: ')
+    search_tuple = (
+        make + '%' if make else '%',
+        model + '%' if model else '%',
+        color + '%' if model else '%',
+        int(year_from) if year_from else 1900,
+        int(year_to) if year_to else 2021,
+        int(price_from) if price_from else 0,
+        int(price_to) if price_to else 1000000
+    )
+    
+    with conn:
+        c.execute(search_query, search_tuple)
+        res = c.fetchall()
+    
+    for i in res:
+        print(i)
+
+while True:
+    choice = input('Search, Insert or Quit? (s/i/q): ')
+    if choice == 's':
+        search()
+    elif choice == 'i':
+        insert()
+    elif choice == 'q':
+        exit()
+    else:
+        print('Input s, i or q!')
