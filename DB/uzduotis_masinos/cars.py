65efd39e08fe6fb42dd68f05102f35178210627e
robotautas <50068472+robotautas@users.noreply.github.com>
2021-01-28 08:35:55 +0200

    db pakeista į cars 1000, ištaisytos klaidelės, pridėtas eilučių skaičiavimas

--- DB/uzduotis_masinos/cars.py
+++ DB/uzduotis_masinos/cars.py
@@ -1,7 +1,7 @@
 import sqlite3
 from sys import exit
 
-conn = sqlite3.connect('cars.db')
+conn = sqlite3.connect('cars1000.db')
 c = conn.cursor()
 
 insert_query = '''
@@ -43,7 +43,7 @@ def search():
     search_tuple = (
         make + '%' if make else '%',
         model + '%' if model else '%',
-        color + '%' if model else '%',
+        color + '%' if color else '%',
         int(year_from) if year_from else 1900,
         int(year_to) if year_to else 2021,
         int(price_from) if price_from else 0,
@@ -56,6 +56,8 @@ def search():
     
     for i in res:
         print(i)
+    print(f'\nTotal {len(res)} rows found.')
+
 
 while True:
     choice = input('Search, Insert or Quit? (s/i/q): ')
